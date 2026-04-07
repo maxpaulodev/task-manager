50c7aa661902c5197267a1b011b22829183be11e
Max Paulo <maxpauloteixeira@outlook.com>
2026-04-06 21:32:14 -0300

    Add simple getters and setters

--- src/com/task/model/Group.java
+++ src/com/task/model/Group.java
@@ -8,9 +8,26 @@ public class Group{
 
     public Group(String name){
         this.name = name;
+        list = new Task[16];
     }
 
 
+    public String getName(){
+        return name;
+    }
+
+    public Task[] getList(){
+        return list;
+    }
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addTask(String taskName){
+
+    }
 
 
 }
