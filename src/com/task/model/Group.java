1f05d58b7c52f1a81df476091e05962144c79f01
Max Paulo <maxpauloteixeira@outlook.com>
2026-04-06 21:17:23 -0300

    Start Group object and lil fixes

---
+++ src/com/task/model/Group.java
@@ -0,0 +1,16 @@
+package com.task.model;
+
+
+public class Group{
+
+    private Task[] list;
+    private String name;
+
+    public Group(String name){
+        this.name = name;
+    }
+
+
+
+
+}
