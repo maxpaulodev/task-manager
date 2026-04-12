5292915238c558c2410cd437b85a0312282c3f49
Max Paulo <maxpauloteixeira@outlook.com>
2026-04-12 19:06:03 -0300

    Create AddTask method to Group

--- src/com/task/model/Group.java
+++ src/com/task/model/Group.java
@@ -5,12 +5,15 @@ public class Group{
 
     private Task[] list;
     private String name;
+    private int groupSize;
+    private int groupMax;
 
     public Group(String name){
         this.name = name;
-        list = new Task[16];
+        groupMax = 16;
+        list = new Task[groupMax];
+        groupSize = 0;
     }
-
 
     public String getName(){
         return name;
@@ -26,7 +29,25 @@ public class Group{
     }
 
     public void addTask(String taskName){
+        Task task = new Task(taskName);
+        list[groupSize] = task;
 
+        groupSize++;
+
+        if (groupSize > groupMax){
+            aumentaMax();
+        }
+    }
+
+
+
+    private void aumentaMax(){
+        groupMax = groupMax + 16;
+        Task[] newList = new Task[groupMax];
+
+        System.arraycopy(list, 0, newList, 0, list.length);
+        
+        list = newList;
     }
 
 
